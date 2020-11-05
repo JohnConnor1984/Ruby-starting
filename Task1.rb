f6ce432c5f32d7c12a7e772d42711f068f1898b6
JohnConnor1984 <73929996+JohnConnor1984@users.noreply.github.com>
2020-11-05 18:25:55 +0300

    Add files via upload

---
+++ Task1.rb
@@ -0,0 +1,25 @@
+﻿range=("a".."c")
+abc=[]
+
+for i in range
+	puts "Введите #{i}:"
+	t=gets.to_i
+	abc.push(t)
+	end
+
+
+D=abc[1]**2-4*abc[0]*abc[2]
+puts"дискриминант равен #{D}"
+if D==0
+	x=-abc[1]/(2*abc[0])
+	puts "x1=x2=#{x}"
+	
+	elsif D>0
+		x1=(-abc[1]+D**0.5)/(2*abc[0])
+		x2=(-abc[1]-D**0.5)/(2*abc[0])
+		puts "x1=#{x1}, x2=#{x2}"
+		
+	elsif D<0
+		puts "Корней нет"
+		end
+		

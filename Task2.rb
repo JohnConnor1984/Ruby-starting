a3fe3990ce24d50595e944cdb9a3c0acc74a401c
JohnConnor1984 <73929996+JohnConnor1984@users.noreply.github.com>
2020-11-06 09:56:16 +0300

    Add files via upload

---
+++ Task2.rb
@@ -0,0 +1,23 @@
+﻿hash = {}
+result = 0
+
+loop do
+  puts "Input the name"
+  name = gets.chomp
+  break if name == "stop"
+  puts "Input the price"
+  price = gets.chomp.to_f
+  puts "Input the number"
+  count = gets.chomp.to_f
+  hash[name] = {"Price" => price, "Number" => count}
+end
+
+result=0
+
+hash.each do |name, price|
+  sum = price["Price"]*price["Number"] 
+  puts "#{name} cost is #{sum}"
+  result += sum	
+end
+
+puts "Total: #{result}"
---
+++ Task3.rb
@@ -0,0 +1,27 @@
+﻿matrix = []
+
+loop do
+  puts "Введите строку матрицы:"
+  str = gets.chomp
+  array = str.split(',')
+  break if (matrix.last == [] and array == [])
+  matrix.push(array)
+end
+
+matrix.delete([])
+
+det = 0
+if matrix.size == matrix[0].size
+  n = matrix.size
+  (0..n-1).each do |i|
+    a_ij = Marshal.load(Marshal.dump(matrix[0][i])).to_i
+    part = Marshal.load(Marshal.dump(matrix))
+    part.delete_at(0)
+    part[0].delete_at(i)
+    minor = part[0][0].to_i
+    minor *= a_ij*(-1)**(2+i)
+    det += minor
+  end
+end
+
+puts "Определитель матрицы: #{det}" 
